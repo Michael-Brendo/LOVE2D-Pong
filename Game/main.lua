09c2631db4040d2b87dfa0a5add513717b413c3d
Michael-Brendo <59937684+Michael-Brendo@users.noreply.github.com>
2020-11-14 15:39:22 -0300

    Update main.lua

--- Game/main.lua
+++ Game/main.lua
@@ -3,9 +3,15 @@ WINDOW_HEIGHT = 720
 
 --[[
     love.load()
-    -Used for initializing the game state at the 
+    -This function is called exactly once at the beginning of the game.
 ]]
+
 function love.load()
+
+--[[
+    love.window.setMode
+    -Sets the display mode and properties of the window.
+]]
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -13,7 +19,15 @@ function love.load()
     })
 end
 
+--[[
+    love.draw()
+    -Called after update by LOVE2D, used to draw on the screen every frame, updated or otherwise.
+]]
+
 function love.draw()
+--[[
+    
+]]
     love.graphics.print("Hello World", 400, 300)
 end
 
